07033138e0409f6016f8bff7ef4ee542303b18be
fabianmeneses <edisonsalamanca30@gmail.com>
2020-10-28 19:39:19 -0500

    docs agregado

---
+++ docs/precache-manifest.efabacbd2736da4904fee16038ffc382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4590803b39f9563e71084280147db88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ca21a87e54b2cede522",
+    "url": "/static/css/main.33f32ae4.chunk.css"
+  },
+  {
+    "revision": "a45c0ff7c9e28121c691",
+    "url": "/static/js/2.3e75c129.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.3e75c129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ca21a87e54b2cede522",
+    "url": "/static/js/main.e829f1f2.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
